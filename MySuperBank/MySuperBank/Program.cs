b38e9f5c2b4c852df14868da1c4c254b03819d6b
mwendwa-will <williammwendwa@gmail.com>
2023-11-01 14:22:49 +0300

    The first part

---
+++ MySuperBank/MySuperBank/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Rein",10000);
+
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+
+
+            account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
+
+            Console.WriteLine(account.GetAccountHistory());
+
+
+
+
+
+
+            //// Test that the initial balances must be positive.
+            //BankAccount invalidAccount;
+            //try
+            //{
+            //    invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //    return;
+            //}
+
+            //// Test for a negative balance.
+            //try
+            //{
+            //    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            //}
+            //catch (InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
+        }
+    }
+}
